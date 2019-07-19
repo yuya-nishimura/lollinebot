0c9178472331c5350e77a4ac8271bf5ba28d00dd
Yuya Nishimura <yuya@YuyaNishimuranoMacBook-Pro.local>
2019-07-19 12:32:11 +0900

    ヒアドキュメント修正2

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -60,19 +60,23 @@ class LinebotController < ApplicationController
     reply_message = {}
 
     if !idol.nil?
-      reply_message["type"] = "text"
-      reply_message["text"] = <<~IDOL
-      名前: #{idol.name}
-      年齢: #{idol.age}歳
-      誕生日: #{idol.birth.month}月#{idol.birth.day}日
-      身長: #{idol.height}cm
-      体重: #{idol.weight}kg
-      利き手: #{idol.handed}
-      3サイズ: #{idol.bwh}
-      IDOL
+      reply_message = {
+        "type": "text",
+        "text": <<~IDOL
+                名前: #{idol.name}
+                年齢: #{idol.age}歳
+                誕生日: #{idol.birth.month}月#{idol.birth.day}日
+                身長: #{idol.height}cm
+                体重: #{idol.weight}kg
+                利き手: #{idol.handed}
+                3サイズ: #{idol.bwh}
+                IDOL
+      }
     else
-      reply_message["type"] = "text"
-      reply_message["text"] = "……"
+      reply_message = {
+        "type": "text"
+        "text": "……"
+      }
     end
   end
 
